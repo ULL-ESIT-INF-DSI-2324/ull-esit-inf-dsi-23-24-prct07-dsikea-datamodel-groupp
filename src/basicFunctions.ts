21a9bf71203f3db00bdebefbc8f3c8dddf5ba5a6
Jose Javier Ramos Carballo <alu0101313313@ull.edu.es>
2024-03-04 01:28:41 +0000

    configuración añadida antes de github actions

---
+++ src/basicFunctions.ts
@@ -0,0 +1,11 @@
+
+/**
+ * the addition of two numbers
+ * @param num1 first number to add
+ * @param num2 second number to add
+ * @returns the addition of the two numbers
+ */
+export function add(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
---
+++ tests/basicFunctions.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import {expect} from 'chai';
+import {add} from '../src/basicFunctions'
+
+describe(`Add tests`, () => {
+  it(`add(1, 2) equals to 3`, () => {
+    expect(add(1,2)).to.be.equal(3);
+  });
+});

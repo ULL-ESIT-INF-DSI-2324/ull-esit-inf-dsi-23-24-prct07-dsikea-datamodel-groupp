eb8ae1698c60bd620bf51fd6f645817fc2673e5e
Daniel Marhuenda <91728552+danielmarhuenda@users.noreply.github.com>
2024-03-07 20:45:12 +0000

    Funciones principales

--- README.md
+++ README.md
@@ -8,6 +8,14 @@
 [![Tests](https://github.com/ULL-ESIT-INF-DSI-2324/ull-esit-inf-dsi-23-24-prct07-dsikea-datamodel-groupp/actions/workflows/node.js.yml/badge.svg)](https://github.com/ULL-ESIT-INF-DSI-2324/ull-esit-inf-dsi-23-24-prct07-dsikea-datamodel-groupp/actions/workflows/node.js.yml)
 
 ## Introducción
+En esta tarea hemos tenido que hacer el gestor de una tienda de muebles, creando las clases mueble, cliente, proveedor y stock:
+
+Mueble tiene su ID único, un nombre común, una descripción, el material del que está hecho, sus dimenciones y su precio. Nada interesante que comentar en este respecto.
+
+Cliente y Proveedor tienen su ID único, su nombre, su contacto y su dirección, no hay diferencias entre las dos clases.
+
+En la clase Stock está todo el trabajo, esta clase tiene tres listas: el stock, dónde se guarda cuantas unidades hay de cada mueble por su ID para seguridad(No nos parecía buena idea que en devolución el supuesto cliente pudiera crear un nuevo mueble propio), muebles, donde se guardan los propios objetos de tipo mueble, y transacciones, donde se guardan las transacciones realizadas.
+
 
 
 ## Tareas Previas
@@ -16,14 +24,19 @@
 - [] Tarea 2
 
 
-## Ejercicios a realizar
+## Ejercicio
 
-### Ejercicio 01
+Las clases Mueble, Cliente y Proveedor no tienen funciones propias más que el constructor ya que no se trabajará modificándolas, sólo guardándolas.
 
+En la clase Stock creamos las funciones privadas AddMueble y QuitarMueble, estas funciones reciben el ID del mueble con el que se trabaja y añaden o quitan una unidad de dicho mueble, si no lo encuentrar dan un error por no existir este.
 
-## Ejercicios sobre la modificación
+La función CrearMueble servirá para añadir un nuevo mueble al Stock, primero comprueba que no existía de antes, y en ese caso lo añade a las listas de stock y de muebles.
 
-### Ejercicio 1
+Para los clientes tenemos las funciones Vender y DevolverCliente, como dicen sus nombres, estas sirven para registrar la venta o la devolución por parte de un cliente, llamando a la función QuitarMueble o AddMueble respectivamente y añadiendolo a la lista de transacciones.
+
+Lo mismo ocurre con los proveedores, las clases Vender y DevolverCliente son iguales a DevolverProveedor y Comprar respectivamente, solamente cambiando donde dice cliente por proveedor.
+
+Esto último se podría hacer con menos código usando una clase padre Persona con Proveedor y Cliente hererando de ella, pero me parecía poco intuitivo que si realizaramos una devolución a un proveedor usaramos la función Vender. Preguntaré esto en la tutoría
 
 
 ## Conclusiones
--- src/stock.ts
+++ src/stock.ts
@@ -1,42 +1,184 @@
 import { Mueble } from "./mueble";
+import { Cliente } from "./cliente";
+import { Proveedor } from "./proveedor";
 
 interface TipoCantidad {
-    mueble:Mueble;
+    MuebleID:number;
     Cantidad:number;
 }
 
+interface Transaccion{
+    Persona:"Cliente"|"Proveedor";
+    MuebleID:number;
+    PersonaID:number;
+    Accion:"Obtener"|"Dar";
+    Fecha:Date;
+    Importe:number;
+}
+
 export class Stock{
-    stock:TipoCantidad[];
+    
+    private stock:TipoCantidad[];
+    private muebles:Mueble[]
+    private transacciones:Transaccion[];
+    private clientes:Cliente[];
+    private proveedores:Proveedor[];
 
     /**
      * Devuelve un mueble a partir de su ID única
      * @param ID Mueble que se está buscando
      * @returns Mueble buscado
      */
-    GetMueble(ID:number):Mueble|undefined{
-        for(var i of this.stock){
-            if(i.mueble.ID == ID){
-                return i.mueble;
+    private GetMueble(ID:number):Mueble|undefined{
+        for(var i of this.muebles){
+            if(i.ID == ID){
+                return i;
             }
         }
 
-        //Mueble no encontrado
+        console.log("Mueble no encontrado");
         return undefined;
     }
 
     /**
-     * Añade una unidad de mueble, si no está se añade a la lista, por eso no puede ser el ID
+     * Añade una unidad de mueble al stock.
      * @param Mueble Mueble a añadir
      */
-    AddMueble(Mueble:Mueble):void{
+    private AddMueble(ID:number):void{
         for(var i of this.stock){
-            if(i.mueble.ID == Mueble.ID){
+            if(i.MuebleID == ID){
                 i.Cantidad++;
+                return;
             }
         }
 
-        this.stock.push({mueble:Mueble, Cantidad:1});
+        console.log("Mueble no encontrado");
+        return;
+    }   
+    
+    /**
+     * Elimina una unidad de mueble del stock
+     * @param ID Mueble a eliminar
+     */
+    private QuitarMueble(ID:number):void{
+        for(var i of this.stock){
+            if(i.MuebleID == ID){
+                if(i.Cantidad > 0){
+                    i.Cantidad--;
+                    return;
+                }else{
+                    console.log("No quedan unidades de ese mueble");
+                    return;
+                }
+            }
+        }
+        console.log("Mueble no encontrado");
+        return;
     }
 
+    /**
+     * Añadir nuevo mueble al stock.
+     * @param mueble Mueble a añadir.
+     */
+    public CrearMueble(mueble:Mueble):void{
+        let existe:boolean = false;
+
+        for(var i of this.stock){
+            if(i.MuebleID == mueble.ID){
+                existe = true;
+            }
+        }
+
+        if(existe == false){
+            this.stock.push({MuebleID:mueble.ID, Cantidad:1});
+            this.muebles.push(mueble);
+            return;
+        }else{
+            console.log("Se intentó añadir mueble ya existente");
+            return;
+        }
+    }
+
+    /**
+     * Cliente compra un mueble, se elimina uno del stock y se guarda la transacción.
+     * @param cliente Cliente que está comprando.
+     * @param ID Mueble que se va a vender.
+     */
+    public Vender(cliente:Cliente, ID:number):void{
+        //Sería poner fecha actual aquí.
+        let fecha:Date = new Date(21-1-2024);
+        let coste:number = this.GetMueble(ID).Precio;
+
+        this.QuitarMueble(ID);
+        this.transacciones.push({Persona:"Cliente", MuebleID:ID, PersonaID:cliente.ID, Accion:"Dar", Fecha:fecha, Importe:coste});
+    }
+
+    /**
+     * Cliente realiza una devolución de un mueble y se guarda la transacción.
+     * @param cliente Cliente que está devolviendo.
+     * @param ID Mueble que se va a devolver.
+     */
+    public DevolverCliente(cliente:Cliente, ID:number):void{
+        //Sería poner fecha actual aquí.
+        let fecha:Date = new Date(21-1-2024);
+        let coste:number = this.GetMueble(ID).Precio;
+
+        this.AddMueble(ID);
+        this.transacciones.push({Persona:"Cliente", MuebleID:ID, PersonaID:cliente.ID, Accion:"Dar", Fecha:fecha, Importe:coste});
+    }
+
+
+    /**
+     * Proveedor vende un mueble, se añade uno al stock y se guarda la transacción.
+     * @param proveedor Proveedor que está vendiendo.
+     * @param ID Mueble que se va a comprar.
+     */
+    public Comprar(proveedor:Proveedor, ID:number):void{
+        //Sería poner fecha actual aquí.
+        let fecha:Date = new Date(21-1-2024);
+        let coste:number = this.GetMueble(ID).Precio;
+
+        this.AddMueble(ID);
+        this.transacciones.push({Persona:"Cliente", MuebleID:ID, PersonaID:proveedor.ID, Accion:"Dar", Fecha:fecha, Importe:coste});
+    }
+
+    /**
+     * Realizamos una devolución de un mueble a un proveedor y se guarda la transacción.
+     * @param proveedor Proveedor al que le devolvemos.
+     * @param ID Mueble que se va a devolver.
+     */
+    public DevolverProveedor(proveedor:Proveedor, ID:number):void{
+        //Sería poner fecha actual aquí.
+        let fecha:Date = new Date(21-1-2024);
+        let coste:number = this.GetMueble(ID).Precio;
+
+        this.QuitarMueble(ID);
+        this.transacciones.push({Persona:"Cliente", MuebleID:ID, PersonaID:proveedor.ID, Accion:"Dar", Fecha:fecha, Importe:coste});
+    }
+
+
+    /**
+     * Crea informes varios sobre el stock o las ventas
+     * @param tipoinforme Tipo de informe que se quiere ver:
+     * 0: Mueble más vendido
+     * 1: Dinero ganado en ventas - Dinero perdido por devoluciones
+     * 2: Dinero gastado en compras - Dinero devuelto por devoluciones
+     * @param ID ID en caso de mirar mueble o persona específica
+     * @returns 
+     */
+    public Informes(tipoinforme:number, ID:number = 0):string{
+
+        if(tipoinforme == 0){
+            let mas_vendido:TipoCantidad = {MuebleID:0, Cantidad:0};
+
+            for(var i of this.stock){
+                if(i.Cantidad >= mas_vendido.Cantidad) mas_vendido = i;
+            }
+
+            return ("Mueble más vendido: " + this.GetMueble(mas_vendido.MuebleID)?.Nombre);
+        }
+
+        return "Tipo de informe no existente";
+    }
 
 }

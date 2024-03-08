f37365c90d6bee077e8e82953af6783dea98e895
Daniel Marhuenda <91728552+danielmarhuenda@users.noreply.github.com>
2024-03-08 16:42:25 +0000

    Test

---
+++ tests/stock.spec.ts
@@ -0,0 +1,17 @@
+import 'mocha';
+import { expect } from 'chai';
+import { Stock } from '../../src/stock';
+import { Mueble } from '../../src/mueble';
+
+let stock = new Stock;
+let mueble:Mueble = new Mueble(1, "silla", "Silla de madera", "madera", [2, 2, 10], 50);
+
+stock.CrearMueble(mueble);
+
+describe("Prueba GetMueble", () => {
+    it("Lo encuentra", () => {
+        expect(stock.GetMueble(1)).to.equal(mueble);
+    });
+
+
+});

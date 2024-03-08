5e723683e5e00755c78fd35ea92d942fce7c4f32
Daniel Marhuenda <91728552+danielmarhuenda@users.noreply.github.com>
2024-03-08 16:43:49 +0000

    Test

--- tests/stock.spec.ts
+++ tests/stock.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha';
 import { expect } from 'chai';
-import { Stock } from '../../src/stock';
-import { Mueble } from '../../src/mueble';
+import { Stock } from '../src/stock';
+import { Mueble } from '../src/mueble';
 
 let stock = new Stock;
 let mueble:Mueble = new Mueble(1, "silla", "Silla de madera", "madera", [2, 2, 10], 50);
